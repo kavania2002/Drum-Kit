9b217ab35ed026e66a92260b7b390c22d0e6a52f
kavania2002 <72624080+kavania2002@users.noreply.github.com>
2020-11-12 00:00:32 +0530

    JavaScript Tutorial
    This was just a Desktop Based Website based on JavaScript

---
+++ index.js
@@ -0,0 +1,64 @@
+var buttons = document.querySelectorAll(".drum");
+
+for (var i=0; i<buttons.length; i++){
+    buttons[i].addEventListener('click', function() {
+        var innerHTML = this.innerHTML;
+        buttonAnimation(innerHTML)
+        makeSound(innerHTML);
+    });
+}
+
+document.addEventListener('keypress', function(event){
+    buttonAnimation(event.key)
+    makeSound(event.key)
+});
+
+function makeSound(key){
+
+    switch (key){
+        case 'w':
+            var tom1 = new Audio('/sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case 'a':
+            var tom2 = new Audio('/sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case 's':
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case 'd':
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case 'j':
+            var crash = new Audio('/sounds/crash.mp3');
+            crash.play();
+            break;
+        
+        case 'k':
+            var kickbass = new Audio('/sounds/kick-bass.mp3');
+            kickbass.play();
+            break;
+        
+        case 'l':
+            var snare = new Audio('/sounds/snare.mp3');
+            snare.play();
+            break;
+  
+    }
+}
+
+function buttonAnimation(ani_key) {
+    var ani_key_sel = document.querySelector('.'+ani_key);
+    ani_key_sel.classList.add('pressed');
+
+    setTimeout(function() {
+        ani_key_sel.classList.remove('pressed')
+    }, 100);
+}
